ba85f4ee4e7e978af8c7b9f3a69a03553e158dfa
Can Okay <canokay2014@gmail.com>
2019-12-04 00:19:01 +0300

    Gravatar code added.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -30,7 +30,7 @@
         {
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.btn_result = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textbox_mail = new System.Windows.Forms.TextBox();
             this.lblMail = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
@@ -38,7 +38,7 @@
             // pictureBox1
             // 
             this.pictureBox1.Location = new System.Drawing.Point(42, 153);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(6);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(600, 600);
             this.pictureBox1.TabIndex = 0;
@@ -47,21 +47,21 @@
             // btn_result
             // 
             this.btn_result.Location = new System.Drawing.Point(42, 78);
-            this.btn_result.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.btn_result.Margin = new System.Windows.Forms.Padding(6);
             this.btn_result.Name = "btn_result";
             this.btn_result.Size = new System.Drawing.Size(600, 63);
             this.btn_result.TabIndex = 1;
             this.btn_result.Text = "Gravatar Inage";
             this.btn_result.UseVisualStyleBackColor = true;
-            this.btn_result.Click += new System.EventHandler(this.button1_Click);
+            this.btn_result.Click += new System.EventHandler(this.btn_result_Click);
             // 
-            // textBox1
+            // textbox_mail
             // 
-            this.textBox1.Location = new System.Drawing.Point(132, 34);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(510, 32);
-            this.textBox1.TabIndex = 2;
+            this.textbox_mail.Location = new System.Drawing.Point(132, 34);
+            this.textbox_mail.Margin = new System.Windows.Forms.Padding(6);
+            this.textbox_mail.Name = "textbox_mail";
+            this.textbox_mail.Size = new System.Drawing.Size(510, 32);
+            this.textbox_mail.TabIndex = 2;
             // 
             // lblMail
             // 
@@ -79,14 +79,14 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(677, 783);
             this.Controls.Add(this.lblMail);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.textbox_mail);
             this.Controls.Add(this.btn_result);
             this.Controls.Add(this.pictureBox1);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
-            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.Margin = new System.Windows.Forms.Padding(6);
             this.Name = "frmMain";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Form1";
+            this.Text = "Gravatar Image";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -97,7 +97,7 @@
 
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Button btn_result;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textbox_mail;
         private System.Windows.Forms.Label lblMail;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -7,6 +7,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using System.Security.Cryptography;
+
 
 namespace GravatarCSharpFormApplication
 {
@@ -17,5 +20,19 @@ namespace GravatarCSharpFormApplication
             InitializeComponent();
         }
 
+        private void btn_result_Click(object sender, EventArgs e)
+        {
+            StringBuilder hash = new StringBuilder();
+            MD5CryptoServiceProvider md5provider = new MD5CryptoServiceProvider();
+            byte[] bytes = md5provider.ComputeHash(new UTF8Encoding().GetBytes(textbox_mail.Text));
+
+            for (int i = 0; i < bytes.Length; i++)
+            {
+                hash.Append(bytes[i].ToString("x2"));
+            }
+            
+
+            pictureBox1.Load("https://www.gravatar.com/avatar/" + hash.ToString() + "?size=600.jpg");
+        }
     }
 }
